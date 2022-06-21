d15dcc4e249ba80f5644cb4b3a8a3287dcbf8fa3
Marco Leonardini <marco_leonardini@hotmail.com>
2022-06-20 22:25:09 -0400

    forms

--- lib/src/screens/signup_screen.dart
+++ lib/src/screens/signup_screen.dart
@@ -5,6 +5,49 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(24),
+        color: Colors.white,
+        width: size.width,
+        height: size.height,
+        child: Column(
+          children: [
+            Text(
+              'Sign Up',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            const SizedBox(height: 32),
+            TextField(
+              decoration: getInputDecoration(hint: 'Name'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              decoration: getInputDecoration(hint: 'Email'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              decoration: getInputDecoration(hint: 'Password'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              decoration: getInputDecoration(hint: 'Password again'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              decoration: getInputDecoration(hint: 'Date of birth'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration getInputDecoration({required String hint}) {
+    return InputDecoration(
+      hintText: hint,
+      border: const OutlineInputBorder(),
+    );
   }
 }
